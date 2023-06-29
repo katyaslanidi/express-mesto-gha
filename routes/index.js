0e3425d437f94f8c98280a166f6fe375af296997
Katya Aslanidi <grechkakat@yandex.ru>
2023-06-29 15:44:28 +0300

    ме

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const { NOT_FOUND } = require('../utils/errors');
 
-app.use(usersRouter);
-app.use(cardsRouter);
+router.use(usersRouter);
+router.use(cardsRouter);
 
 router.use((req, res) => {
   return res.status(NOT_FOUND.error_code).send({ message: NOT_FOUND.message });
