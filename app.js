f7bc0356de690b1f017a9b2a3f921d61a0683395
Katya Aslanidi <grechkakat@yandex.ru>
2023-07-06 17:39:10 +0300

    исправила ошибки app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const { errors } = require('celebrate');
-const router = require('./routes');
 const auth = require('./middlewares/auth');
 const authRoutes = require('./routes/auth');
-// const { login, registration } = require('./controllers/users');
-
+const cardsRoutes = require('./routes/cards');
+const usersRoutes = require('./routes/users');
+const router = require('./routes/index');
+const { NOT_FOUND } = require('./utils/errors');
 
 const app = express();
 
@@ -17,12 +18,22 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/', authRoutes);
+app.use(auth);
+app.use('/users', usersRoutes);
+app.use('/cards', cardsRoutes);
+app.use(router);
+app.use((req, res, next) => next(new NOT_FOUND('Страницы по данному url не существует')));
+app.use((errors()));
+app.listen(PORT, () => {
+  console.log(`Сервер запущен, PORT = ${PORT}`);
+});
 // app.post('/signin', login);
 // app.post('/signup', registration);
-app.use(authRoutes);
-app.use(auth);
-app.use(router);
-app.use(errors());
+// app.use(authRoutes);
+// app.use(auth);
+// app.use(router);
+// app.use(errors());
 // app.use((err, req, res, next) => {
 //   const { statusCode = 500, message } = err;
 //   res.status(statusCode)
@@ -31,7 +42,3 @@ app.use(errors());
 //     });
 //     next();
 // });
-
-app.listen(PORT, () => {
-  console.log(`Сервер запущен, PORT = ${PORT}`);
-});
