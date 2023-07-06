0299038efb5eef7836104bcfe3f8622535e68232
Katya Aslanidi <grechkakat@yandex.ru>
2023-07-06 19:18:11 +0300

    исправила ошибки app.js _8

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors/errors');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers.get('Set-Cookie');
+  const { authorization } = req.headers.get.cookie;
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
