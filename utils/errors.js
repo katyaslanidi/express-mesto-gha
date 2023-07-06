36c7ad8d4117be8343ba59257b3fdf68073b82be
Katya Aslanidi <grechkakat@yandex.ru>
2023-07-06 17:50:42 +0300

    исправила ошибки app.js _2

--- utils/errors.js
+++ utils/errors.js
@@ -1,41 +1,43 @@
-module.exports = class BAD_REQUEST extends Error {
+class BAD_REQUEST extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 400;
   }
 };
 
-module.exports = class NOT_FOUND extends Error {
+class NOT_FOUND extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 404;
   }
 };
 
-module.exports = class INTERNAL_SERVER_ERROR extends Error {
+class INTERNAL_SERVER_ERROR extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 500;
   }
 };
 
-module.exports = class CONFLICT_ERROR extends Error {
+class CONFLICT_ERROR extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 409;
   }
 };
 
-module.exports = class UNAUTHORIZED_ERROR extends Error {
+class UNAUTHORIZED_ERROR extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 401;
   }
 };
 
-module.exports = class FORBIDDEN_ERROR extends Error {
+class FORBIDDEN_ERROR extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 403;
   }
 };
+
+module.exports = { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR, CONFLICT_ERROR, UNAUTHORIZED_ERROR, FORBIDDEN_ERROR };
